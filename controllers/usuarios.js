2ee16468d8dc282fd6b39f14490531c86562dc60
Jose Bedoya <jbedoya2001@hotmail.com>
2020-10-10 13:24:47 -0500

    Fin seccion 13 corregido

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,26 +2,20 @@ const { response } = require('express');
 const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
-const usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+
 
 const getUsuarios = async(req, res) => {
 
     const desde = Number(req.query.desde) || 0;
 
-    // const usuarios = await Usuario
-    //                         .find({}, ' nombre email role google')
-    //                         .skip( desde )
-    //                         .limit( 5 );
-    // const total = await Usuario.count();
-
-    const [usuarios, total] = await Promise.all([
+    const [ usuarios, total ] = await Promise.all([
         Usuario
-            .find({}, ' nombre email role google img')
+            .find({}, 'nombre email role google img')
             .skip( desde )
             .limit( 5 ),
 
-        Usuario.count()
+        Usuario.countDocuments()
     ]);
 
 
@@ -30,134 +24,150 @@ const getUsuarios = async(req, res) => {
         usuarios,
         total
     });
+
 }
 
 const crearUsuario = async(req, res = response) => {
 
-    const { password, email } = req.body;
+    const { email, password } = req.body;
 
-     try {
+    try {
 
         const existeEmail = await Usuario.findOne({ email });
 
         if ( existeEmail ) {
-            res.status(400).json({
+            return res.status(400).json({
                 ok: false,
-                msg: 'Error: El email ya existe '
+                msg: 'El correo ya está registrado'
             });
         }
-        
+
         const usuario = new Usuario( req.body );
-
-        // encriptar contraseña
+    
+        // Encriptar contraseña
         const salt = bcrypt.genSaltSync();
-        usuario.password = bcrypt.hashSync( password, salt )
-
-        // guardar usuario
+        usuario.password = bcrypt.hashSync( password, salt );
+    
+    
+        // Guardar usuario
         await usuario.save();
 
-        // Generar Token
-        const token = await generarJWT(usuario.id);
+        // Generar el TOKEN - JWT
+        const token = await generarJWT( usuario.id );
+
+
         res.json({
             ok: true,
             usuario,
             token
-        });  
+        });
+
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
             ok: false,
-            msg: 'Error inesperado... revisar Logs '
+            msg: 'Error inesperado... revisar logs'
         });
     }
 
+
 }
 
-const actualizarUsuario = async(req, res = response) => {
+
+const actualizarUsuario = async (req, res = response) => {
+
     // TODO: Validar token y comprobar si es el usuario correcto
+
     const uid = req.params.id;
 
+
     try {
-        
+
         const usuarioDB = await Usuario.findById( uid );
 
-        if(!usuarioDB) {
+        if ( !usuarioDB ) {
             return res.status(404).json({
                 ok: false,
-                msg: 'Usuario no encontrado '
+                msg: 'No existe un usuario por ese id'
             });
         }
 
         // Actualizaciones
         const { password, google, email, ...campos } = req.body;
 
-        if( usuarioDB.email !== email ) {
-            const existeEmail = await Usuario.findOne({email});
+        if ( usuarioDB.email !== email ) {
+
+            const existeEmail = await Usuario.findOne({ email });
             if ( existeEmail ) {
                 return res.status(400).json({
                     ok: false,
-                    msg: 'Ya existe un usuario con ese email '
+                    msg: 'Ya existe un usuario con ese email'
                 });
             }
         }
-
-        if (!usuarioDB.google) {
-            campos.email = email;
-        } else if (usuarioDB.email !== email ) {
-            return res.status(400).json({
-                ok: false,
-                msg: 'Usuario de Google no puede cambiar su correo'
-            });
-        }
         
-        const usuarioActualizado = await usuario.findByIdAndUpdate( uid, campos, { new: true } );
-
+        campos.email = email;
+        const usuarioActualizado = await Usuario.findByIdAndUpdate( uid, campos, { new: true } );
 
         res.json({
             ok: true,
             usuario: usuarioActualizado
-        });         
+        });
+
+        
     } catch (error) {
         console.log(error);
-        return res.status(500).json({
+        res.status(500).json({
             ok: false,
-            msg: 'Error inesperado... revisar Logs '
-        });
+            msg: 'Error inesperado'
+        })
     }
+
 }
 
-const borrarUsuario = async(req, res = response) => {
+
+const borrarUsuario = async(req, res = response ) => {
+
     const uid = req.params.id;
+
     try {
 
         const usuarioDB = await Usuario.findById( uid );
 
-        if(!usuarioDB) {
+        if ( !usuarioDB ) {
             return res.status(404).json({
                 ok: false,
-                msg: 'Usuario no encontrado '
+                msg: 'No existe un usuario por ese id'
             });
         }
 
         await Usuario.findByIdAndDelete( uid );
 
-        return res.status(500).json({
+        
+        res.json({
             ok: true,
             msg: 'Usuario eliminado'
-        });        
+        });
+
     } catch (error) {
+        
         console.log(error);
-        return res.status(500).json({
+        res.status(500).json({
             ok: false,
-            msg: 'Error inesperado... revisar Logs '
-        });       
+            msg: 'Hable con el administrador'
+        });
+
     }
+
 
 }
 
+
+
 module.exports = {
-      getUsuarios, 
-      crearUsuario,
-      actualizarUsuario,
-      borrarUsuario
+    getUsuarios,
+    crearUsuario,
+    actualizarUsuario,
+    borrarUsuario
 }
